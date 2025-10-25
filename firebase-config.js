b5c25427bc96304971d2079675c19dd845962ef1
dou28 <55640652+dou28@users.noreply.github.com>
2025-10-25 21:34:26 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,18 @@
 // firebase-config.js
-firebase.initializeApp({
+
+// Configuration Firebase (remplace les valeurs par celles de ton projet)
+const firebaseConfig = {
   apiKey: "AIzaSyD4O388ofZ9ZT6F0KDEjbcjc4gPNEyOxNc",
   authDomain: "bf6-loadouts.firebaseapp.com",
   projectId: "bf6-loadouts",
   storageBucket: "bf6-loadouts.firebasestorage.app",
   messagingSenderId: "1003913644717",
-  appId: "1:1003913644717:web:7c8868f3fe838e2d82d424",
+  appId: "1:1003913644717:web:7c8868f3fe838e2d82d424"
+};
 
-});
+// Initialisation de Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase
+// Initialisation des services
 const auth = firebase.auth();
 const db = firebase.firestore();
